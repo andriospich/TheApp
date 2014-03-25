94289adf4ca02efa004067f2c9f1e8ae47946ee4
andriospich <aspich@mdsol.com>
2014-03-25 13:18:26 +0000

    Knows about football now

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -7,6 +7,11 @@ public class QueryProcessor {
             return "Computer programming is the comprehensive process that leads from an original " 
                    + "formulation of a computing problem to executable programs.";
         }
+        else
+            if (query.contains("football")) {
+                return "The best player in the world is ronaldo...after Zidane.";
+            }
+
         return "";
     }
 }
