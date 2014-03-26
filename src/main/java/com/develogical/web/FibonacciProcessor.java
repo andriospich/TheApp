94f1f95206d6290993b82b82b76e9d17dbb9d3fe
Nicolas Bétheuil <nicolas.betheuil@valtech.fr>
2014-03-26 14:16:57 +0100

    add fibonacci processor

---
+++ src/main/java/com/develogical/web/FibonacciProcessor.java
@@ -0,0 +1,27 @@
+package com.develogical.web;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import com.develogical.IQueryProcessor;
+
+public class FibonacciProcessor implements IQueryProcessor {
+
+    private final String[] fibonacci;
+
+    public FibonacciProcessor() {
+        fibonacci = "0	1	1	2	3	5	8	13	21	34	55	89	144	233	377	610	987".split("\t");
+    }
+    public boolean matchRequest(String query) {
+        return query.toLowerCase().contains("fibonacci");
+    }
+
+    public String result(String query) {
+        Pattern compile = Pattern.compile(".*? (\\d)+th");
+        Matcher matcher = compile.matcher(query);
+        if (matcher.find()) {
+            int group = Integer.parseInt(matcher.group(1));
+            return String.valueOf(fibonacci[group]);
+        }
+        return "";
+    }
+}
